3aec39866107ec2105cbbc1f5b1e48fb6eea4022
@欧柏泉 <baiquan.ou@jimubox.com>
2016-12-15 12:27:25 +0800

    添加对cocoapods的支持。

--- TangramKitTests/TangramKitTests.swift
+++ TangramKitTests/TangramKitTests.swift
@@ -27,9 +27,9 @@ class TangramKitTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+//        self.measure({
+//            
+//        })
     }
     
 }
