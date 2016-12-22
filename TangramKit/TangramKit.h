901fd0fd89c772e671670bc83d48ed5e826d1c1e
hanwei <1019260351@qq.com>
2016-12-22 12:57:39 +0800

    支持Carthage

---
+++ TangramKit/TangramKit.h
@@ -0,0 +1,19 @@
+//
+//  TangramKit.h
+//  TangramKit
+//
+//  Created by 韩威 on 2016/12/22.
+//  Copyright © 2016年 youngsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TangramKit.
+FOUNDATION_EXPORT double TangramKitVersionNumber;
+
+//! Project version string for TangramKit.
+FOUNDATION_EXPORT const unsigned char TangramKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TangramKit/PublicHeader.h>
+
+
